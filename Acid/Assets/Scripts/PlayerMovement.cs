916b175cf0d4de09107027e38384758778ea3b15
Vendelin <vkabicek@gmail.com>
2018-04-21 11:35:30 +0200

    playerMove

---
+++ Acid/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float direction;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("w"))
+        {
+            transform.position += new Vector3(0f, direction, 0f);
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            transform.position += new Vector3(0f, -direction, 0f);
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            transform.position += new Vector3(-direction, 0f, 0f);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            transform.position += new Vector3(direction, 0f, 0f);
+        }
+    }
+
+    
+}
